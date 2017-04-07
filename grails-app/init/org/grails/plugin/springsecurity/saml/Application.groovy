9c9803176df311919be8f06f8e82918de05083b4
Jeff Wilson <jeffwils@cisco.com>
2017-04-06 22:25:48 -0400

    Version 3.1.0 - Fixes Gorm Incompatibility for Grails 3.1+

--- grails-app/init/org/grails/plugin/springsecurity/saml/Application.groovy
+++ grails-app/init/org/grails/plugin/springsecurity/saml/Application.groovy
@@ -3,8 +3,22 @@ package org.grails.plugin.springsecurity.saml
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
+import static grails.util.Metadata.getCurrent
+import static grails.util.Metadata.getCurrent
+import static grails.util.Metadata.getCurrent
+
+import static grails.util.Metadata.current as metaInfo
+
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
+
+        println "App version ${metaInfo.getApplicationVersion()}"
+        println "App name ${metaInfo.getApplicationName()}"
+        println "Grails version ${metaInfo.getGrailsVersion()}"
+        println "Groovy version ${GroovySystem.version}"
+        println "JVM version ${System.getProperty('java.version')}"
+
+
         GrailsApp.run(Application, args)
     }
 }
