ce4a00fc538d058f3048b29664682f7a22dabb14
I Stevenson <irstevenson@gmail.com>
2018-04-11 11:42:07 +1000

    #14 - Integration tests now green. And so testing now works again.
    That is, you'll get a full pass with:
    
        ./gradlew clean check
    
    Had to fix up dependencies and config for datasources - see build.gradle
    and application.yml. And tweaked the single integration spec to be more
    inline with current grails version being used.

--- src/integration-test/groovy/org/grails/plugin/springsecurity/saml/SpringSamlUserDetailsServiceIntegrationSpec.groovy
+++ src/integration-test/groovy/org/grails/plugin/springsecurity/saml/SpringSamlUserDetailsServiceIntegrationSpec.groovy
@@ -1,12 +1,10 @@
 package org.grails.plugin.springsecurity.saml
 
-import grails.test.mixin.integration.Integration
-import org.junit.Test
-import org.springframework.transaction.annotation.Transactional
+import grails.testing.mixin.integration.Integration
+import grails.transaction.Rollback
 import spock.lang.Specification
 import test.TestUserRole
 import test.TestSamlUser
-import grails.test.mixin.TestFor
 import test.TestRole
 import org.springframework.security.saml.SAMLCredential
 import org.opensaml.saml2.core.impl.NameIDImpl
@@ -14,33 +12,47 @@ import org.opensaml.saml2.core.impl.AssertionImpl
 import grails.core.GrailsApplication
 
 @Integration
-@Transactional
-@TestFor(SpringSamlUserDetailsService)
+@Rollback
 class SpringSamlUserDetailsServiceIntegrationSpec extends Specification {
 
     String username = "jackSparrow"
 
     GrailsApplication grailsApplication
-    @Test
+
     def "Test getting user details from db"() {
         given:
-        TestSamlUser user = TestSamlUser.build([username:username,email:'bob@fake.com'])
-        TestRole role = TestRole.build(authority:"testauth")
-        TestUserRole userRole = TestUserRole.build(user:user,role:role)
-        SpringSamlUserDetailsService service = new SpringSamlUserDetailsService(samlAutoAssignAuthorities: false,samlAutoCreateActive: true,userDomainClassName: "test.TestSamlUser",samlAutoCreateKey: 'username',authorityNameField: 'authority',authorityJoinClassName: 'test.TestUserRole')
-        service.grailsApplication = grailsApplication
+            TestSamlUser user = new TestSamlUser(
+                    username:username,
+                    email:'bob@fake.com',
+                    password: 'test'
+            ).save( failOnError: true )
+            TestRole role = new TestRole(authority:"testauth").save( failOnError: true )
+            TestUserRole.create( user,  role )
+
+            SpringSamlUserDetailsService service = new SpringSamlUserDetailsService(
+                    samlAutoAssignAuthorities: false,
+                    samlAutoCreateActive: true,
+                    userDomainClassName: "test.TestSamlUser",
+                    samlAutoCreateKey: 'username',
+                    authorityNameField: 'authority',
+                    authorityJoinClassName: 'test.TestUserRole')
+            service.grailsApplication = grailsApplication
 
         when:
-        SAMLCredential cred
-        cred = new SAMLCredential(new NameIDImpl("", "", ""), new AssertionImpl("", "", ""), null, null)
-        cred.metaClass.getNameID = { [value: "$username"] }
-        def loadedUser = service.loadUserBySAML(cred)
+            SAMLCredential cred
+            cred = new SAMLCredential(
+                    new NameIDImpl("", "", ""),
+                    new AssertionImpl("", "", ""),
+                    null,
+                    null)
+            cred.metaClass.getNameID = { [value: "$username"] }
+            def loadedUser = service.loadUserBySAML(cred)
 
         then:
-        user.username == username && user.email == 'bob@fake.com' && loadedUser && loadedUser.username == username
-
+            user.username == username
+            user.email == 'bob@fake.com'
+            loadedUser
+            loadedUser.username == username
     }
-
-
 
 }
