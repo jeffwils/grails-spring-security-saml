c7a2f55a768e9598baaa13f65a8918cc54bc0061
I Stevenson <irstevenson@gmail.com>
2018-06-04 11:34:54 +1000

    Add integration test for useLocalRoles
    Involved a little refactor of existing test to keep things DRY.
    
    Issue: #43

--- src/integration-test/groovy/org/grails/plugin/springsecurity/saml/SpringSamlUserDetailsServiceIntegrationSpec.groovy
+++ src/integration-test/groovy/org/grails/plugin/springsecurity/saml/SpringSamlUserDetailsServiceIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package org.grails.plugin.springsecurity.saml
 
 import grails.testing.mixin.integration.Integration
 import grails.transaction.Rollback
+import org.springframework.security.core.userdetails.UserDetails
 import spock.lang.Specification
 import test.TestUserRole
 import test.TestSamlUser
@@ -15,16 +16,21 @@ import grails.core.GrailsApplication
 @Rollback
 class SpringSamlUserDetailsServiceIntegrationSpec extends Specification {
 
-    String username = "jackSparrow"
-
     GrailsApplication grailsApplication
+
+    private final String testUsername = 'jackSparrow'
+    private final String testEmail = 'bob@fake.com'
+    private final String testPassword = 'test'
+    private final String userDomainClassName = "test.TestSamlUser"
+    private final String authorityNameField = 'authority'
+    private String authorityJoinClassName = 'test.TestUserRole'
 
     def "Test getting user details from db"() {
         given:
             TestSamlUser user = new TestSamlUser(
-                    username:username,
-                    email:'bob@fake.com',
-                    password: 'test'
+                    username: testUsername,
+                    email:    testEmail,
+                    password: testPassword
             ).save( failOnError: true )
             TestRole role = new TestRole(authority:"testauth").save( failOnError: true )
             TestUserRole.create( user,  role )
@@ -32,27 +38,61 @@ class SpringSamlUserDetailsServiceIntegrationSpec extends Specification {
             SpringSamlUserDetailsService service = new SpringSamlUserDetailsService(
                     samlAutoAssignAuthorities: false,
                     samlAutoCreateActive: true,
-                    userDomainClassName: "test.TestSamlUser",
+                    userDomainClassName: userDomainClassName,
                     samlAutoCreateKey: 'username',
-                    authorityNameField: 'authority',
-                    authorityJoinClassName: 'test.TestUserRole')
+                    authorityNameField: authorityNameField,
+                    authorityJoinClassName: authorityJoinClassName)
             service.grailsApplication = grailsApplication
 
         when:
-            SAMLCredential cred
-            cred = new SAMLCredential(
-                    new NameIDImpl("", "", ""),
-                    new AssertionImpl("", "", ""),
-                    null,
-                    null)
-            cred.metaClass.getNameID = { [value: "$username"] }
-            def loadedUser = service.loadUserBySAML(cred)
+            UserDetails loadedUser = (UserDetails)service.loadUserBySAML( buildSamlCredential( testUsername ) )
 
         then:
-            user.username == username
-            user.email == 'bob@fake.com'
+            user.username == testUsername
+            user.email == testEmail
             loadedUser
-            loadedUser.username == username
+            loadedUser.username == testUsername
     }
 
+    def 'Test retrieval of user roles from local DB'() {
+        given: 'A user with some roles and configuration to use local roles'
+            TestSamlUser user = new TestSamlUser(
+                    username: testUsername,
+                    email: testEmail,
+                    password: testPassword
+            ).save( failOnError: true )
+            ['role1', 'role2', 'role3', 'role4'].each { roleName ->
+                def role = new TestRole( authority: roleName ).save( failOnError: true )
+                TestUserRole.create( user, role )
+            }
+            SpringSamlUserDetailsService service = new SpringSamlUserDetailsService(
+                    samlUseLocalRoles:         true, // the key configuration param
+                    samlAutoAssignAuthorities: false,
+                    samlAutoCreateActive:      false,
+                    userDomainClassName:       userDomainClassName,
+                    authorityNameField:        authorityNameField,
+                    authorityJoinClassName:    authorityJoinClassName )
+            service.grailsApplication = grailsApplication
+
+        when: 'We attempt to retrieve the user based on SAML Credentials'
+            UserDetails loadedUser = (UserDetails)service.loadUserBySAML( buildSamlCredential( testUsername ) )
+            def loadedRoles = TestSamlUser.findByUsername( testUsername ).authorities
+
+        then: 'the user is returned with roles which match their DB roles'
+            loadedUser.username == testUsername
+            loadedUser.authorities.size() == loadedRoles.size()
+            loadedUser.authorities*.authority.containsAll( loadedRoles*.authority )
+    }
+
+    private static SAMLCredential buildSamlCredential(String username ) {
+        SAMLCredential cred
+        cred = new SAMLCredential(
+                new NameIDImpl("", "", ""),
+                new AssertionImpl("", "", ""),
+                null,
+                null)
+        cred.metaClass.getNameID = { [value: "$username"] }
+
+        cred
+    }
 }
